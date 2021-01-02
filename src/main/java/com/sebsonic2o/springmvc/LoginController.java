d2c213c148dc25f386e5569d856c95545d71a39d
Sebastien Cheraux <scheraux@gmail.com>
2021-01-01 16:00:44 -0800

    redirect to welcome page passing (name) param on post with spring mvc

--- src/main/java/com/sebsonic2o/springmvc/LoginController.java
+++ src/main/java/com/sebsonic2o/springmvc/LoginController.java
@@ -1,13 +1,22 @@
 package com.sebsonic2o.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/login")
+	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String doGet() {
 		return "login";
+	}
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String doPost(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
+		return "welcome";
 	}
 }
